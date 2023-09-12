e6058ff6665c55cf97e4d7bd606790ca7c1b8e67
linkon77921 <89232583+linkon77921@users.noreply.github.com>
2023-09-12 23:53:30 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// Application Run Here
+
+const app = require("./app");
+const dotenv = require('dotenv')
+dotenv.config({path:'./config.env'});
+
+
+app.listen(process.env.RUNNING_PORT,()=>{
+    console.log("server is running here");
+});
